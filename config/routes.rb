dc961113ce7a2a76c7750a812c2c32f4a2dfed12
DawidCwiek <cwiekdawid1@gmail.com>
2019-11-14 17:47:07 +0100

    add msg and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   get 'home', to: 'home#index'
+  get 'central/:id', to: 'centrals#show'
 end
