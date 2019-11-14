ba14d731343aed2fcbc337dfeca47d5203f98044
DawidCwiek <cwiekdawid1@gmail.com>
2019-11-14 20:02:33 +0100

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :repo_url, 'git@github.com:DawidCwiek/PKZ.git'
 
 set :stages,       %w(production)
 
-set :linked_files, %w(config/database.yml config/secrets.yml)
+set :linked_files, %w(config/database.yml config/master.key)
 
 set :linked_dirs,  %w(log vendor/bundle tmp/sockets tmp/pids tmp/cache)
 
