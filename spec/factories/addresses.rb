baa8736b5b7ef4c0105494c484b809c3e4d5dc5b
dawidcwiek <cwiekdawid1@gmail.com>
2019-11-20 17:59:48 +0100

    change centrals controller

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :address do
-    city { Faker::Address.cit }
+    city { Faker::Address.city }
     street { Faker::Address.street_address }
     zip_code { Faker::Address.zip_code }
   end
