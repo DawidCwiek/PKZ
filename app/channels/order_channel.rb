a1111051f09ffdb9cd072bd7512a74ce94c83f12
dawidcwiek <cwiekdawid1@gmail.com>
2020-01-09 14:59:08 +0100

    ws end

--- app/channels/order_channel.rb
+++ app/channels/order_channel.rb
@@ -2,5 +2,12 @@ class OrderChannel < ApplicationCable::Channel
   def subscribed
     store = Store.find(params[:store_id])
     stream_from "store_#{store.id}"
+    data
+  end
+
+  private
+
+  def data
+    ActionCable.server.broadcast "store_#{params[:store_id]}", data: Store.find(params[:store_id]).orders.where(issued: false)
   end
 end
